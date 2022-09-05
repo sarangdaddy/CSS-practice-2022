ac06bc86d14ff16aaae3b5ab0dc29e5bdc903141
Sung Un KIM <ksu8904@gmail.com>
2022-09-05 23:20:40 +0900

    random game

--- test2/js.js
+++ test2/js.js
@@ -1,44 +1,25 @@
-var members = [
-  "손흥민",
-  "김영권",
-  "이승우",
-  "기성용",
-  "황희찬",
-  "황희조",
-  "구자철",
-  "이용",
-  "조현우",
-  "김승규",
-  "이강인",
-  "박주호",
-  "문선민",
-  "김민재",
-  "황인범",
-  "이정협",
-  "김인성",
-  "구성윤",
-  "장현수",
-  "나상호",
-];
+const gameForm = document.querySelector(".game__form");
+const rangeNumberInput = gameForm.querySelector(".range__number input");
+const guessNumberInput = gameForm.querySelector(".guess__number input");
+const chosenForm = document.querySelector(".chosen__form");
+const resultForm = document.querySelector(".result__form");
 
-function showNotAssigneds() {
-  var membersString = "";
-  for (var i = 0; i < members.length; i++) {
-    membersString += members[i];
-    if (i < members.length - 1) membersString += ", ";
+function getResult(machineNumber, guessNumber) {
+  if (machineNumber === guessNumber) {
+    chosenForm.innerText = `You chose ${guessNumber} Machine chose ${machineNumber} `;
+    resultForm.innerText = `You won!!!`;
+  } else {
+    chosenForm.innerText = `You chose ${guessNumber} Machine chose ${machineNumber} `;
+    resultForm.innerText = `You lose.`;
   }
-  document.querySelector("#not_assigned").innerHTML = membersString;
 }
 
-showNotAssigneds();
-
-function assignMemberToTeam(teamNumber) {
-  if (members.length === 0) return;
-  var member = members[0];
-  member = members.splice(0, 1);
-  showNotAssigneds();
-
-  var liEl = document.createElement("li");
-  liEl.append(member);
-  document.querySelector("#team_" + teamNumber).append(liEl);
+function makeRandomNum(event) {
+  event.preventDefault();
+  const rangeNumber = rangeNumberInput.value;
+  const machineNumber = Math.ceil(Math.random() * rangeNumber);
+  const guessNumber = guessNumberInput.value;
+  getResult(parseInt(machineNumber), parseInt(guessNumber));
 }
+
+gameForm.addEventListener("submit", makeRandomNum);
